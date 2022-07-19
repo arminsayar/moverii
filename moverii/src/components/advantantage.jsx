277393b6048a09c57bedf16531d99fa3aafac3a1
arminsayar <saya.armin4@gmail.com>
2022-07-19 22:09:18 +0430

    new section added

--- moverii/src/components/advantantage.jsx
+++ moverii/src/components/advantantage.jsx
@@ -2,55 +2,55 @@ const Advantantage = () => {
   return (
     <>
       <div className="adv-first">
-        <h1>Advantantage</h1>
+        <h1>Advantantages</h1>
         <div className="adv-main">
-          <div className="box-style">
+          <div className="box-style box1">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box1-ico"><img src="./files/01.png"/></span>Advantantage 1<span className="box1-arr"><img src="./files/Shape.png"/></span>
             </h3>
           </div>
-          <div className="box-style">
+          <div className="box-style box2">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box2-ico"><img src="./files/02.png"/></span>Advantantage 2<span className="box2-arr"><img src="./files/Shape.png"/></span>
             </h3>
           </div>
-          <div className="box-style">
+          <div className="box-style box3">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box3-ico"><img src="./files/03.png"/></span>Advantantage 3<span className="box3-arr"><img src="./files/Shape.png"/></span>
             </h3>
           </div>
         </div>
         <div className="adv-main">
-          <div className="box-style">
+          <div className="box-style box4">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box4-ico"><img src="./files/04.png"/></span>Advantantage 4<span className="box4-arr"><img src="./files/Shape.png"/></span>
             </h3>
           </div>
-          <div className="box-style">
+          <div className="box-style box5">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box5-ico"><img src="./files/05.png"/></span>Advantantage 5<span className="box5-arr"><img src="./files/Shape.png"/></span>
             </h3>
           </div>
-          <div className="box-style">
+          <div className="box-style box6">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box6-ico"><img src="./files/06.png"/></span>Advantantage 6<span className="box6-arr"><img src="./files/Shape.png"/></span>
             </h3>
           </div>
         </div>
         <div className="adv-main">
-          <div className="box-style">
+          <div className="box-style box7">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box7-ico"><img src="./files/7.png"/></span>Advantantage 7<span className="box7-arr"><img src="./files/Shape.png"/></span>
             </h3>
           </div>
-          <div className="box-style">
+          <div className="box-style box8">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box8-ico"><img src="./files/8.png"/></span >Advantantage 8<span className="box8-arr"><img src="./files/Shape.png"/></span>
             </h3>
           </div>
-          <div className="box-style">
+          <div className="box-style box9">
             <h3>
-              <span>logo</span>Advantantage 1<span>--</span>
+              <span className="box9-text">Register now</span><span className="box9-arr"><img src="./files/5.png"/></span>
             </h3>
           </div>
         </div>
--- moverii/src/components/header.jsx
+++ moverii/src/components/header.jsx
@@ -10,11 +10,11 @@ const Header = () => {
             <li className="dutch-li">
             <span className="dutch-ico"><img src="./files/image5.png"/></span> Dutch <span className="dutch-arrow"><img src="./files/Shape.png"/></span>
             </li>
-            <li className="ml-16 fav-li"><span className="fav-ico"><img src="./files/1.png"/></span>Favorite</li>
-            <li className="ml-16 help-li"><span className="help-ico"><img src="./files/2.png"/></span>help</li>
-            <li className="ml-16 search-li"><span className="search-ico"><img src="./files/3.png"/></span>search</li>
-            <li className="ml-16 acc-li"><span className="acc-ico"><img src="./files/4.png"/></span>account</li>
-            <li className="ml-16 button-background sign-button">sign in</li>
+            <li className="ml-16 fav-li"><span className="fav-ico"><img src="./files/1.png"/></span>Favoriten</li>
+            <li className="ml-16 help-li"><span className="help-ico"><img src="./files/2.png"/></span>Hilfe</li>
+            <li className="ml-16 search-li"><span className="search-ico"><img src="./files/3.png"/></span>Suche</li>
+            <li className="ml-16 acc-li"><span className="acc-ico"><img src="./files/4.png"/></span>Anmelden</li>
+            <li className="ml-16 button-background sign-button">Registrieren</li>
           </ul>
         </nav>
       </header>
